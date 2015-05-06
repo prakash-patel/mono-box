6f9537c96f9d441cd65651c7cf9cb2799f87847c
Prakash Patel <pakpatel@hotmail.com>
2015-05-05 22:31:48 -0500

    issue#5: update version

--- puppet/profile/manifests/libgdiplus.pp
+++ puppet/profile/manifests/libgdiplus.pp
@@ -1,19 +1,20 @@
 #== Class: profile::gdiplus
 
 class profile::libgdiplus(
-  $libgdiplus_version = ''
+  $libgdiplus_version = '',
+  $libgdipuls_extension =''
 )inherits profile {
 
   exec{'retrieve_gdiplus_pkg':
-    command => "wget http://origin-download.mono-project.com/sources/libgdiplus/libgdiplus-${libgdiplus_version}.tar.gz",
+    command => "wget http://origin-download.mono-project.com/sources/libgdiplus/${libgdiplus_version}.${libgdipuls_extension}",
     cwd     => '/usr/src',
     path    => "/usr/sbin:/usr/bin:/sbin:/bin",
     require => Class['profile::mono'],
-    unless  => "ls /usr/src/libgdiplus-${libgdiplus_version}.tar.gz",
+    unless  => "ls /usr/src/${libgdiplus_version}.${libgdipuls_extension}",
   }
 
   exec{ 'extract_gdiplus_pkg':
-    command     => "tar -xvzf libgdiplus-${libgdiplus_version}.tar.gz",
+    command     => "tar -xvzf ${libgdiplus_version}.${libgdipuls_extension}",
     cwd         => '/usr/src',
     path        => "/usr/sbin:/usr/bin:/sbin:/bin",
     subscribe   => Exec['retrieve_gdiplus_pkg'],
@@ -21,8 +22,8 @@ class profile::libgdiplus(
   }
 
   exec{'configure_gdiplus':
-    command     => "/usr/src/libgdiplus-${libgdiplus_version}/./configure --prefix=/usr",
-    cwd         => "/usr/src/libgdiplus-${libgdiplus_version}",
+    command     => "/usr/src/${libgdiplus_version}/./configure --prefix=/usr",
+    cwd         => "/usr/src/${libgdiplus_version}",
     path        => "/usr/sbin:/usr/bin:/sbin:/bin",
     subscribe   => Exec['extract_gdiplus_pkg'],
     refreshonly => true,
@@ -31,7 +32,7 @@ class profile::libgdiplus(
 
   exec{'make_gdiplus':
     command     => "make",
-    cwd         => "/usr/src/libgdiplus-${libgdiplus_version}",
+    cwd         => "/usr/src/${libgdiplus_version}",
     subscribe   => Exec['configure_gdiplus'],
     path        => "/usr/sbin:/usr/bin:/sbin:/bin",
     timeout     => 0,
@@ -41,7 +42,7 @@ class profile::libgdiplus(
 
   exec{'install_gdiplus':
     command     => "make install",
-    cwd         => "/usr/src/libgdiplus-${libgdiplus_version}",
+    cwd         => "/usr/src/${libgdiplus_version}",
     subscribe   => Exec['make_gdiplus'],
     path        => "/usr/sbin:/usr/bin:/sbin:/bin",
     timeout     => 0,
--- puppet/profile/manifests/xsp.pp
+++ puppet/profile/manifests/xsp.pp
@@ -1,20 +1,19 @@
 # == Class: profile/xsp
 class profile::xsp (
-  $xsp_version = ''
+  $xsp_version = '',
+  $xsp_extension = ''
 )inherits profile{
 
-  $version = '2.10'
-
   exec{'retrieve_xsp_pkg':
-    command => "wget http://origin-download.mono-project.com/sources/xsp/xsp-${xsp_version}.tar.bz2",
+    command => "wget http://origin-download.mono-project.com/sources/xsp/${xsp_version}.${xsp_extension}",
     cwd     => '/usr/src',
     path    => "/usr/sbin:/usr/bin:/sbin:/bin",
     require => Class['profile::libgdiplus'],
-    unless  => "ls /usr/src/xsp-${xsp_version}.tar.bz2",
+    unless  => "ls /usr/src/${xsp_version}.${xsp_extension}",
   }
 
   exec{ 'extract_xsp_pkg':
-    command     => "tar -xvjf xsp-${xsp_version}.tar.bz2",
+    command     => "tar -xvjf ${xsp_version}.${xsp_extension}",
     cwd         => '/usr/src',
     subscribe   => Exec['retrieve_xsp_pkg'],
     path        => "/usr/sbin:/usr/bin:/sbin:/bin",
@@ -23,8 +22,8 @@ class profile::xsp (
   }
 
   exec{'configure_xsp':
-    command => "/usr/src/xsp-${xsp_version}/./configure --prefix=/usr",
-    cwd         => "/usr/src/xsp-${xsp_version}",
+    command => "/usr/src/${xsp_version}/./configure --prefix=/usr",
+    cwd         => "/usr/src/${xsp_version}",
     path        => "/usr/sbin:/usr/bin:/sbin:/bin",
     subscribe   => Exec['extract_xsp_pkg'],
     user        => 'root',
@@ -34,7 +33,7 @@ class profile::xsp (
 
   exec{'make_xsp':
     command     => "su -c 'make'",
-    cwd         => "/usr/src/xsp-${xsp_version}",
+    cwd         => "/usr/src/${xsp_version}",
     environment => ["PKG_CONFIG_PATH=/usr/lib/pkgconfig"],
     subscribe   => Exec['configure_xsp'],
     user        => 'root',
@@ -45,7 +44,7 @@ class profile::xsp (
 
   exec{'install_xsp':
     command     => "su -c 'make install'",
-    cwd         => "/usr/src/xsp-${xsp_version}",
+    cwd         => "/usr/src/${xsp_version}",
     subscribe   => Exec['make_xsp'],
     environment => ["PKG_CONFIG_PATH=/usr/lib/pkgconfig"],
     path        => "/usr/sbin:/usr/bin:/sbin:/bin",
